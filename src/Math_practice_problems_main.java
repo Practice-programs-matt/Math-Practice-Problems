88f1f71eb84138a996999dcf6a6da2abe0388f3b
mpearce25 <pearcematthew26@gmail.com>
2014-11-08 18:59:48 -0500

    began working on input selection

--- src/Math_practice_problems_main.java
+++ src/Math_practice_problems_main.java
@@ -1,11 +1,24 @@
+import java.util.*;
 
 public class Math_practice_problems_main {
 
 	public static void main(String args[])
 	{
-		// github is awesome
-		System.out.println("test");
-		System.out.println("Alex");
+		char addition= 0;
+		char subtraction = 0;
+		System.out.println("What kind of math would you like to practice today. You can type addition, subtraction, multiplication, or division");
+		Scanner scan = new Scanner(System.in);
+		String problem_type = scan.next();
+		
+		if (problem_type = "addition")
+		{
+			
+		}
+		
+		if (problem_type != "addition" || "subtraction" || "multiplication" || "division")
+		{
+			System.out.println("Whoops! Please try again, you may have type something wrong or " + problem_type +" is not yet supported.");
+		}
 	}
 	
 }
--- src/addition.java
+++ src/addition.java
@@ -1,4 +1,12 @@
+import java.util.*;
+public class addition 
+{
 
-public class addition {
-
+	public static void main (String args[])
+	{
+		Random rand = new Random();
+		System.out.println((rand.nextInt(5)+5));
+	}
+	
+	
 }
