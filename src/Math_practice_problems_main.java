5fa0e1b0d65d232017802c838511c82042bdf1b8
mpearce25 <pearcematthew26@gmail.com>
2014-11-08 21:06:55 -0500

    updated

--- src/Math_practice_problems_main.java
+++ src/Math_practice_problems_main.java
@@ -10,8 +10,6 @@ public class Math_practice_problems_main {
 		Scanner scan = new Scanner(System.in);
 		String problem_type = scan.next();
 		
-	
-		
 		if (problem_type.equalsIgnoreCase("addition") || problem_type.equalsIgnoreCase("subtraction") ||problem_type.equalsIgnoreCase("division") || problem_type.equalsIgnoreCase("multiplication"))
 		{
 			System.out.println("Great, let's beigin practicing " + problem_type + "!" );
