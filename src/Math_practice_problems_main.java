69f06abfcea41ae7de90cd4d9cce3313b2e031ef
mpearce25 <pearcematthew26@gmail.com>
2014-11-08 22:41:09 -0500

    Added input selection
    still not working

--- src/Math_practice_problems_main.java
+++ src/Math_practice_problems_main.java
@@ -4,20 +4,27 @@ public class Math_practice_problems_main {
 
 	public static void main(String args[])
 	{
-		char addition= 0;
-		char subtraction = 0;
-		System.out.println("What kind of math would you like to practice today. You can type addition, subtraction, multiplication, or division");
-		Scanner scan = new Scanner(System.in);
-		String problem_type = scan.next();
+
+		String problem_type;
+		do 
+		{
+			System.out.println("What kind of math would you like to practice today. You can type addition, subtraction, multiplication, or division");
+			Scanner scan = new Scanner(System.in);
+			problem_type = scan.next();
+			
+			if (!problem_type.equalsIgnoreCase("addition") || !problem_type.equalsIgnoreCase("subtraction") ||!problem_type.equalsIgnoreCase("division") || !problem_type.equalsIgnoreCase("multiplication"))
+			{
+				System.out.println("Whoops! You may have typed something wrong or " + problem_type + " is not yet supported");
+			}
+			else 
+			{
+				System.out.println("Great, we will begin practicing " + problem_type);
+			}
+		}
+		while (!problem_type.equalsIgnoreCase("addition") || !problem_type.equalsIgnoreCase("subtraction") ||!problem_type.equalsIgnoreCase("division") || !problem_type.equalsIgnoreCase("multiplication"));
 		
-		if (problem_type.equalsIgnoreCase("addition") || problem_type.equalsIgnoreCase("subtraction") ||problem_type.equalsIgnoreCase("division") || problem_type.equalsIgnoreCase("multiplication"))
-		{
-			System.out.println("Great, let's beigin practicing " + problem_type + "!" );
-		}
-		else 
-		{
-			System.out.println("Whoops! Please try again, you may have type something wrong or " + "'" + problem_type + "' " + "is not yet supported.");
-		}
+		
+	
 	}
 	
 }
