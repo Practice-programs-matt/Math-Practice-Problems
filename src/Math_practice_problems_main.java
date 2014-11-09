3407a8692605b364f8d6ff8f7c03c57ab4c99427
mpearce25 <pearcematthew26@gmail.com>
2014-11-08 19:36:11 -0500

    changes

--- src/Math_practice_problems_main.java
+++ src/Math_practice_problems_main.java
@@ -12,9 +12,8 @@ public class Math_practice_problems_main {
 		
 		if (problem_type = "addition")
 		{
+			
 		}
-		//real comment
-		//second comment
 		
 		if (problem_type != "addition" || "subtraction" || "multiplication" || "division")
 		{
