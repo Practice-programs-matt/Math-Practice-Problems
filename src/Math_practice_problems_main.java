83a1c04d8345eb83daf0b5572b0583b9581df5f4
mpearce25 <pearcematthew26@gmail.com>
2014-11-08 23:13:11 -0500

    Got input selection working

--- src/Math_practice_problems_main.java
+++ src/Math_practice_problems_main.java
@@ -4,27 +4,6 @@ public class Math_practice_problems_main {
 
 	public static void main(String args[])
 	{
-
-		String problem_type;
-		do 
-		{
-			System.out.println("What kind of math would you like to practice today. You can type addition, subtraction, multiplication, or division");
-			Scanner scan = new Scanner(System.in);
-			problem_type = scan.next();
-			
-			if (!problem_type.equalsIgnoreCase("addition") || !problem_type.equalsIgnoreCase("subtraction") ||!problem_type.equalsIgnoreCase("division") || !problem_type.equalsIgnoreCase("multiplication"))
-			{
-				System.out.println("Whoops! You may have typed something wrong or " + problem_type + " is not yet supported");
-			}
-			else 
-			{
-				System.out.println("Great, we will begin practicing " + problem_type);
-			}
-		}
-		while (!problem_type.equalsIgnoreCase("addition") || !problem_type.equalsIgnoreCase("subtraction") ||!problem_type.equalsIgnoreCase("division") || !problem_type.equalsIgnoreCase("multiplication"));
-		
-		
-	
+		select_problems.select();
 	}
-	
 }
---
+++ src/select_problems.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+
+public class select_problems 
+{
+	public static void select()
+	{
+		String problem_type;
+		String number_of_problems;
+		String problem_verb = null;
+		int largest_number;
+		
+		Scanner scan = new Scanner(System.in);
+		do 
+		{
+			System.out.println("What kind of math would you like to practice today. You can type addition, subtraction, multiplication, or division");
+			
+			problem_type = scan.next();
+			
+			if (!problem_type.equalsIgnoreCase("addition") && !problem_type.equalsIgnoreCase("subtraction") && !problem_type.equalsIgnoreCase("division") && !problem_type.equalsIgnoreCase("multiplication"))
+			
+			{
+				System.out.println("Whoops! You may have typed something wrong or " + problem_type + " is not yet supported");
+			}
+			else 
+			{
+				System.out.println("Great, we will begin practicing " + problem_type);
+				
+			}
+		}
+		while (!problem_type.equalsIgnoreCase("addition") && !problem_type.equalsIgnoreCase("subtraction") && !problem_type.equalsIgnoreCase("division") && !problem_type.equalsIgnoreCase("multiplication"));
+		
+		
+		if (problem_type.equalsIgnoreCase("addition"))
+		{
+			problem_verb = "add";
+		}
+		
+		if (problem_type.equalsIgnoreCase("division"))
+		{
+			problem_verb = "divide";
+		}
+		
+		if (problem_type.equalsIgnoreCase("subtraction"))
+		{
+			problem_verb = "subtract";
+		}
+		
+		if (problem_type.equalsIgnoreCase("multiplication"))
+		{
+			problem_verb = "multiply";
+		}
+		
+		System.out.println("How many problems do you want to to try?");
+		number_of_problems = scan.next();
+		
+		System.out.println("What are the largest numbers you want to " + problem_verb);
+		largest_number = scan.nextInt();
+		
+		
+	
+	}
+	
+	
+}
